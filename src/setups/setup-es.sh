fb232d0975fb76a3c1e609d108bd794489970828
DarkNuke090 <DarkNuke090@users.noreply.github.com>
2021-03-03 13:50:49 +0000

    Setup! 📣

--- src/setups/setup-es.sh
+++ src/setups/setup-es.sh
@@ -111,7 +111,8 @@ while true; do
                 echo -e '\e[93m     att: Nuke'
                     sleep 5
             clear
-                echo -e '\e[91m     Saliendo...'; break;;     
+                echo -e '\e[91m     Saliendo...'; break;;
+                    sleep 1.5    
         * ) 
             clear
                 echo -e "\e[91m     Opcion no valida!"

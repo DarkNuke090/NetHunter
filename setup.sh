9e15d515aa11f054c83082590482e918e4750693
DarkNuke090 <DarkNuke090@users.noreply.github.com>
2021-06-08 17:53:16 +0100

    Prueba

--- setup.sh
+++ setup.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 # Hola! si copias codigo da creditos 😜🌴
 
+####################### Variables 💡 #######################
+
+DIRECTORIO_IDIOMA=/data/data/com.termux/files/home/NetHunter/src/idioma
+
 ####################### Inicio 🍕 #######################
 
+   if [ -d "$DIRECTORIO_IDIOMA" ]; then
+      echo -e 'u'
+   else
+      echo -e 'a'
+   fi
+   sleep 10
 clear
     echo -e '\e[91m                  Amplia la pantalla con los dedos para verlo mejor'
         sleep 2.5
